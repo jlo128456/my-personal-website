02d67e34b0825a6368aca9405d63e849effb518a
Jeffrey <j.lo128456@gmail.com>
2024-04-15 22:04:51 +1000

    added Java script function and content in resume

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+<<<<<<< HEAD
 //user click on show more and open popup
+=======
+>>>>>>> c3759c2 (added Java script function and content in resume)
 function popUpList() 
 {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+<<<<<<< HEAD
 }
+=======
+}
+
+function popUpList2() 
+{
+    var popup2 = document.getElementById("myPopup2");
+    popup2.classList.toggle("show");
+   
+}
+>>>>>>> c3759c2 (added Java script function and content in resume)
