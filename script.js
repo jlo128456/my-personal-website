8375b0a448230b0fcc77ad7d6a6e6cf5499800c4
Jeffrey <j.lo128456@gmail.com>
2024-04-17 22:01:21 +1000

    Update

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function popUpList2()
     popup2.classList.toggle("show");
    
 }
+
+function popUpList3() 
+{
+    var popup3 = document.getElementById("myPopup3");
+    popup3.classList.toggle("show");
+   
+}
+function popUpList4() 
+{
+    var popup4 = document.getElementById("myPopup3");
+    popup4.classList.toggle("show");
+   
+}
